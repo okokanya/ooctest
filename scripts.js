499be8d2eb5924f0b288b679deaa53f4a47ad98f
anya <anya@MacBook-Pro-anya.local>
2019-11-28 08:54:39 +0300

    codestyle

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var crossToPush = document.getElementById('crossToPush');
 var blockClass = document.getElementById('blockClass');
 
 function changeHeight () {
-    blockClass.style.height === "fit-content" ?  blockClass.style.height = "0px" : blockClass.style.height="fit-content";
-    blockClass.style.width === "fit-content" ?  blockClass.style.width = "0px" : blockClass.style.width="fit-content"
+    blockClass.style.height === "180px" ?  blockClass.style.height = "0px" : blockClass.style.height="180px";
+    blockClass.style.width === "140px" ?  blockClass.style.width = "0px" : blockClass.style.width="140px";
 };
 
